e72bbad96ca7df91c4c0cbd57fc040cbda4a9991
kyle kersey <kylelk@comcast.net>
2016-04-05 07:49:58 -0700

    removing files for rewrite

--- src/data_source.ads
+++
@@ -1,8 +0,0 @@
-with SQLite;
-
-package Data_Source is
-   procedure Load(DB_Conn: in out SQLite.Data_Base);
-   procedure Save(DB_Conn: in out SQLite.Data_Base);
-
-   function Current_Namespace return String;
-end Data_Source;
