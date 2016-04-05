62e1e343643f942484f2e98fcbc6c06f8a9d3c22
kyle kersey <kylelk@comcast.net>
2016-04-05 12:58:08 -0700

    removed unused code in main.adb

--- src/main.adb
+++ src/main.adb
@@ -7,30 +7,20 @@ with Ada.Strings.Fixed;
 with Ada.Strings.Unbounded;
 with Ada.Characters.Handling;
 with GNAT.Directory_Operations;
-with Ada.IO_Exceptions;
-with Ada.Command_Line;
 
 -- project imports
 with config;
 with file_operations;
-with file_sha1;
-with Status;
 
 procedure main is
    use Ada.Directories;
    use Ada.Text_IO;
    use Ada.Characters.Handling;
 
-   package CLI renames Ada.Command_Line;
    package TIO renames Ada.Text_IO;
    package DIR_OPS renames GNAT.Directory_Operations;
-   package UBS renames Ada.Strings.Unbounded;
 
-   Project_Status : Status.Status_Map.Map;
 
-   function "+"
-     (S : String) return UBS.Unbounded_String is
-     (UBS.To_Unbounded_String (S));
 
    function Integer2Hexa
      (Hex_Int : Integer;
